6c2bc85e8145ff6f0268872fadca9bb4f1b1baa2
sengsovanndy <47348537+sengsovanndy@users.noreply.github.com>
2019-02-14 14:10:07 +0700

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,17 @@
+#include 
+main()
+{
+  int a,temp;
+  printf("enter the number to check the given number is even or odd");
+  scanf("%d",&a);
+  temp=a/2;
+  if(temp==0);
+   {
+    printf("the given numbe is even");
+   }
+   else
+   {
+    printf("the given number is odd");
+    }
+  
+}
